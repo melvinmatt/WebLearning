259c2ada792bcb4b13d6e27b2db589e7bbb91ff4
mlmtt1998 <mattmell98@gmail.com>
2020-09-18 10:19:02 +0530

    Validated Post

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,9 +1,43 @@
 const exp = require("express");
 const router = exp.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
 
-// @route GET api/post/test
-// @desc Tests posts route
-// @access Public
+// Post model
+const Post = require("../../models/Post");
+
+// Validation
+const validatePostInput = require("../../validation/post");
+
+// @route   GET api/post/test
+// @desc    Tests posts route
+// @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Posts works!" }));
+
+// @route   POST api/posts
+// @desc    Create post
+// @access  Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // If any errors, send 400 with error objects
+      return res.status(404).json(errors);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id,
+    });
+
+    newPost.save().then((post) => res.json(post));
+  }
+);
 
 module.exports = router;
