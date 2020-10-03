81af30b0ba898ea6975849e51f4f662b478287fd
mlmtt1998 <mattmell98@gmail.com>
2020-10-03 15:22:38 +0530

    Used till login that can carry the token

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
+import errReducer from "./errorReducer";
 
 export default combineReducers({
   auth: authReducer,
+  errors: errReducer,
 });
