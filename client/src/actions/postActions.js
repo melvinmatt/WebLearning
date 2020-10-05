c5e333b3d7950c94fb81fd229ab5ac6753a2d899
mlmtt1998 <mattmell98@gmail.com>
2020-10-06 01:58:47 +0530

    Post feed

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,6 +1,13 @@
 import axios from "axios";
 
-import { ADD_POST, GET_ERRORS } from "./types";
+import {
+  ADD_POST,
+  DELETE_POST,
+  GET_ERRORS,
+  GET_POSTS,
+  GET_POST,
+  POST_LOADING,
+} from "./types";
 
 // Add post
 export const addPost = (postData) => (dispatch) => {
@@ -18,4 +25,111 @@ export const addPost = (postData) => (dispatch) => {
         payload: err.response.data,
       })
     );
+};
+
+// Get posts
+export const getPosts = () => (dispatch) => {
+  dispatch(setPostLoading());
+  axios
+    .get("/api/post")
+    .then((res) =>
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null,
+      })
+    );
+};
+
+// Get post
+export const getPost = (id) => (dispatch) => {
+  dispatch(setPostLoading());
+  axios
+    .get(`/api/post/${id}`)
+    .then((res) =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch({
+        type: GET_POST,
+        payload: null,
+      })
+    );
+};
+
+// Delete Post
+export const deletePost = (id) => (dispatch) => {
+  axios
+    .delete(`/api/post/${id}`)
+    .then((res) =>
+      dispatch({
+        type: DELETE_POST,
+        payload: id,
+      })
+    )
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
+
+// Add like
+export const addLike = (id) => (dispatch) => {
+  axios
+    .post(`/api/post/like/${id}`)
+    .then((res) => dispatch(getPosts()))
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
+
+// Remove like
+export const removeLike = (id) => (dispatch) => {
+  axios
+    .post(`/api/post/unlike/${id}`)
+    .then((res) => dispatch(getPosts()))
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
+
+// Add comment
+export const addComment = (postId, commentData) => (dispatch) => {
+  axios
+    .post(`/api/post/comment/${postId}`, commentData)
+    .then((res) =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
+
+// Set loading state
+export const setPostLoading = () => {
+  return {
+    type: POST_LOADING,
+  };
 };
