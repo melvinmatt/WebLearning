2ddcbeafc085bf45f3b05a3957fc5d2200aef8b6
mlmtt1998 <mattmell98@gmail.com>
2020-10-06 13:16:11 +0530

    Wrap-Up

--- config/key.js
+++ config/key.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURL:
-    "mongodb+srv://mel_jsrr:jsrracademydb@cluster0.y32bp.mongodb.net/mel_jsrr?retryWrites=true&w=majority",
-  secretOrKey: "secret",
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
