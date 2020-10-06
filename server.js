5b9449085bf142b449f95922129181d5f2539215
mlmtt1998 <mattmell98@gmail.com>
2020-10-06 13:53:42 +0530

    Prepared for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const exp = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const user = require("./routes/api/user");
 const profile = require("./routes/api/profile");
@@ -28,9 +29,20 @@ app.use(passport.initialize());
 // Passport Config
 require("./config/passport")(passport);
 
+// Use routes
 app.use("/api/user", user);
 app.use("/api/profile", profile);
 app.use("/api/post", posts);
+
+// Server static assets  if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
