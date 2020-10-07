d81dd12ae109aa8da10a80c3de4cfcb022efb474
mlmtt1998 <mattmell98@gmail.com>
2020-10-07 13:30:33 +0530

    Prepared for deployment

---
+++ client/build/precache-manifest.41f0a253fcc5bd2e23a8e86bcb81c55d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76569f33913d927b3834f3516c152b01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb0e27253e691855d5a5",
+    "url": "/static/css/main.48389a3a.chunk.css"
+  },
+  {
+    "revision": "a7c2bccb48c53c57e30a",
+    "url": "/static/js/2.a52b9135.chunk.js"
+  },
+  {
+    "revision": "306f3bf9d2f1ee8399ad560a7731d0d6",
+    "url": "/static/js/2.a52b9135.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb0e27253e691855d5a5",
+    "url": "/static/js/main.6026a0a2.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  },
+  {
+    "revision": "ec93a7b3b2502e693ad5b8688dabe3ff",
+    "url": "/static/media/spinner.ec93a7b3.gif"
+  }
+]);
