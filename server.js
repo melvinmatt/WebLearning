90821306f7505ccf1fd5b4f6cfc0d0363ccec354
mlmtt1998 <mattmell98@gmail.com>
2020-10-07 12:26:33 +0530

    Prepared for deployment

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(exp.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
   });
 }
 
