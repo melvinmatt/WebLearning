42d159df4fcc4a77cac77bc0c913d30ded204e94
mlmtt1998 <mattmell98@gmail.com>
2020-10-27 16:24:52 +0530

    Check for working without redux

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const exp = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
-const path = require("path");
 
 const user = require("./routes/api/user");
 const profile = require("./routes/api/profile");
